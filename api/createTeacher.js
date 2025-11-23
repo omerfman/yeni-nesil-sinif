5c7051c63a0a41d1451c4b0c07e3a92f59eb1470
omerfman <omerfarukadam.tr@gmail.com>
2025-11-24 02:19:48 +0300

    fix: vercel api createTeacher export default for CORS

--- api/createTeacher.js
+++ api/createTeacher.js
@@ -14,7 +14,7 @@ if (!admin.apps.length) {
 const auth = admin.auth();
 const db = admin.firestore();
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -77,4 +77,4 @@ module.exports = async (req, res) => {
       error: error.message || 'Öğretmen hesabı oluşturulurken hata oluştu',
     });
   }
-};
+}
